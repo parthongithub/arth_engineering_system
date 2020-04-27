97cdfbf77d72ec3ee829a9fcfedc1b7f16b8c517
Parth <parth_org@outlook.com>
2020-04-27 14:19:50 +0530

    Add new reactor page in products.

---
+++ js/pages/menu.js
@@ -0,0 +1,99 @@
+// var mainMenu =
+//   '<nav role="navigation" class="collapse navbar-collapse navbar main-nav" > <ul class="nav navbar-nav navbar-left">   <!-- <li><a href="index.html">Home</a></li> -->   <li class="dropdown">     <a href="about.html">About us</a>     <ul class="dropdown-menu">       <li><a href="#">Our profile</a></li>       <li><a href="#">Network</a></li>       <li>         <a href="#">Quality assurance and control</a>       </li>       <li><a href="#">Vision and mission</a></li>       <li><a href="#">Product catalogue</a></li>       <li>         <a href="#">Testimonial and certificate</a>       </li>     </ul>   </li>   <!-- <li class="dropdown">     <a href="" hred="#">Credential</a>     <ul class="dropdown-menu">       <li>         <a href="#">Testimonial and certificate</a>       </li>     </ul>   </li> -->   <li class="dropdown">     <a href="service.html">Products</a>     <ul class="dropdown-menu">       <li>         <!-- <a href="tanks.html">Reactor/Pressure vessel</a> -->         <a href="pages/products/reactor.html">Reactor/Pressure vessel</a>       </li>       <li>         <a href="stacks.html"           >Heat exchanger/condenser</a         >       </li>       <li>         <a href="duct.html"           >Finned tube heat exchanger</a         >       </li>       <li>         <a href="pipe.html">Distillation column</a>       </li>       <li><a href="angle.html">Ribbon blender</a></li>       <li>         <a href="beam.html">Storage equipments</a>       </li>       <li>         <a href="beam.html">Hot/Cold water system</a>       </li>     </ul>   </li>   <li><a href="contact.html">Clientele</a></li>   <li><a href="contact.html">Careers</a></li>   <li><a href="contact.html">Contact us</a></li> </ul> <!-- /navbar-nav -->  <div class="navbar-right">   <!-- <div class="header-search">     <form>       <input         type="search"         name="search"         placeholder="Search"       />       <button type="submit">         <i class="fa fa-search" aria-hidden="true"></i>       </button>     </form>   </div> --> </div> </nav>';' +
+
+var mainMenu =
+  "<nav" +
+  '  role="navigation"' +
+  '  class="collapse navbar-collapse navbar main-nav"' +
+  ">" +
+  '  <ul class="nav navbar-nav navbar-left">' +
+  '    <!-- <li><a href="index.html">Home</a></li> -->' +
+  '    <li class="dropdown">' +
+  '      <a href="about.html">About us</a>' +
+  '      <ul class="dropdown-menu">' +
+  '        <li><a href="#">Our profile</a></li>' +
+  '        <li><a href="#">Network</a></li>' +
+  "        <li>" +
+  '          <a href="#">Quality assurance and control</a>' +
+  "        </li>" +
+  '        <li><a href="#">Vision and mission</a></li>' +
+  '        <li><a href="#">Product catalogue</a></li>' +
+  "        <li>" +
+  '          <a href="#">Testimonial and certificate</a>' +
+  "        </li>" +
+  "      </ul>" +
+  "    </li>" +
+  '    <!-- <li class="dropdown">' +
+  '      <a href="" hred="#">Credential</a>' +
+  '      <ul class="dropdown-menu">' +
+  "        <li>" +
+  '          <a href="#">Testimonial and certificate</a>' +
+  "        </li>" +
+  "      </ul>" +
+  "    </li> -->" +
+  '    <li class="dropdown">' +
+  '      <a href="service.html">Products</a>' +
+  '      <ul class="dropdown-menu">' +
+  "        <li>" +
+  '          <!-- <a href="tanks.html">Reactor/Pressure vessel</a> -->' +
+  '          <a href="pages/products/reactor.html">Reactor/Pressure vessel</a>' +
+  "        </li>" +
+  "        <li>" +
+  '          <a href="stacks.html"' +
+  "            >Heat exchanger/condenser</a" +
+  "          >" +
+  "        </li>" +
+  "        <li>" +
+  '          <a href="duct.html"' +
+  "            >Finned tube heat exchanger</a" +
+  "          >" +
+  "        </li>" +
+  "        <li>" +
+  '          <a href="pipe.html">Distillation column</a>' +
+  "        </li>" +
+  '        <li><a href="angle.html">Ribbon blender</a></li>' +
+  "        <li>" +
+  '          <a href="beam.html">Storage equipments</a>' +
+  "        </li>" +
+  "        <li>" +
+  '          <a href="beam.html">Hot/Cold water system</a>' +
+  "        </li>" +
+  "      </ul>" +
+  "    </li>" +
+  '    <li><a href="contact.html">Clientele</a></li>' +
+  '    <li><a href="contact.html">Careers</a></li>' +
+  '    <li><a href="contact.html">Contact us</a></li>' +
+  "  </ul>" +
+  "  <!-- /navbar-nav -->" +
+  "" +
+  '  <div class="navbar-right">' +
+  '    <!-- <div class="header-search">' +
+  "      <form>" +
+  "        <input" +
+  '          type="search"' +
+  '          name="search"' +
+  '          placeholder="Search"' +
+  "        />" +
+  '        <button type="submit">' +
+  '          <i class="fa fa-search" aria-hidden="true"></i>' +
+  "        </button>" +
+  "      </form>" +
+  "    </div> -->" +
+  "  </div>" +
+  "</nav>";
+
+var productMenuForSidebar =
+  '<div class="col-md-3">' +
+  '  <div class="left-sidebar">' +
+  "    <h4>Products</h4>" +
+  "    <ul>" +
+  '      <li><a href="#">Reactor/Pressure vessel</a></li>' +
+  '      <li><a href="#">Heat exchanger/condenser</a></li>' +
+  '      <li><a href="#">Finned tube heat exchanger</a></li>' +
+  '      <li><a href="#">Distillation column</a></li>' +
+  '      <li><a href="#">Ribbon blender</a></li>' +
+  '      <li><a href="#">Storage equipments</a></li>' +
+  '      <li><a href="#">Hot/Cold water system</a></li>' +
+  "    </ul>" +
+  "  </div>" +
+  "</div>";
---
+++ js/pages/temp.js
@@ -0,0 +1,14 @@
+<div class="col-md-3">
+  <div class="left-sidebar">
+    <h4>Products</h4>
+    <ul>
+      <li><a href="#">Reactor/Pressure vessel</a></li>
+      <li><a href="#">Heat exchanger/condenser</a></li>
+      <li><a href="#">Finned tube heat exchanger</a></li>
+      <li><a href="#">Distillation column</a></li>
+      <li><a href="#">Ribbon blender</a></li>
+      <li><a href="#">Storage equipments</a></li>
+      <li><a href="#">Hot/Cold water system</a></li>
+    </ul>
+  </div>
+</div>
